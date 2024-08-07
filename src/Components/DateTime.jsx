3518c9ad66a3d6ea456fcad0066b0b0353944730
unknown <shannellechua@gmail.com>
2024-08-07 23:12:44 +0800

    Adding hover on login button

--- src/Components/DateTime.jsx
+++ src/Components/DateTime.jsx
@@ -1,4 +1,5 @@
 // Credit: https://medium.com/create-a-clocking-in-system-on-react/create-a-react-app-displaying-the-current-date-and-time-using-hooks-21d946971556
+//This is a component to automatically a date and time in a display such as: January 1, 2000 12:00:00
 
 import  React, { useState , useEffect } from 'react'
 
